e21eca54cc894d597f6741c94dbd56a92ad2235c
Julia Ikhsanova <j.ikhsanova@gmail.com>
2025-09-04 18:17:25 +0300

    HW5

---
+++ homework/julia_ikhsanova/hw5/task1.py
@@ -0,0 +1,13 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+
+print(type(person))
+
+name, last_name, city, phone, country = person
+print(name)
+print(last_name)
+print(city)
+print(phone)
+print(country)
+
+
+
