25e9acdf9d5149f334ac33ac264f1616315b7395
Julia Ikhsanova <j.ikhsanova@gmail.com>
2025-09-05 13:09:40 +0300

    HW6

---
+++ homework/julia_ikhsanova/hw6/task1.py
@@ -0,0 +1,15 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+
+words = text.split()  # делим на слова
+
+fin_text = []
+
+for word in words:
+    if word.endswith((".", ",")):
+        fin_text.append(word[:-1] + "ing" + word[-1])  # вернули знак обратно
+    else:
+        fin_text.append(word + "ing")
+
+print(" ".join(fin_text))
+
+
