56632213b68d6caf4b399eb4ba839474f953ffe7
Julia Ikhsanova <j.ikhsanova@gmail.com>
2025-09-09 17:05:08 +0300

    hw7

--- homework/julia_ikhsanova/hw7/task3.py
+++ homework/julia_ikhsanova/hw7/task3.py
@@ -1,21 +1,21 @@
 def process_line(line):
     print(int(line.split()[-1]) + 10)
 
+
 lines = [
     "результат операции: 42",
     "результат операции: 54",
     "результат работы программы: 209",
-    "результат: 2"
+    "результат: 2",
 ]
 
-for l in lines:
-    process_line(l)
+for line in lines:
+    process_line(line)
 
 # a = "результат операции: 42"
 # qa = a.split()
 # qaz = int(qa[-1])
 # print(qaz + 10)
-#
 #
 # b = "результат операции: 514"
 # qb = int(b.split(":")[1])
@@ -24,5 +24,4 @@ for l in lines:
 # c = "результат работы программы: 9"
 # qc = int(c.split(":")[1])
 # print(10 + qc)
-
 
