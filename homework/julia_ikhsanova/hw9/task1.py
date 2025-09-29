2a1210e743499e310592275b0885236c9b4f2328
Julia Ikhsanova <j.ikhsanova@gmail.com>
2025-09-29 19:47:19 +0300

    hw9

--- homework/julia_ikhsanova/hw9/task1.py
+++ homework/julia_ikhsanova/hw9/task1.py
@@ -5,11 +5,5 @@ date_str = "Jan 15, 2023 - 12:05:33"
 dt = datetime.strptime(date_str, "%b %d, %Y - %H:%M:%S")
 
 print(dt.strftime("%B"))   # January
-
 print(dt.strftime("%d.%m.%Y, %H:%M"))
-
-
-
-
-
 
