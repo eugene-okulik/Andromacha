c5031b46d94862036b7546aff0de97b2597e3ad8
Julia Ikhsanova <j.ikhsanova@gmail.com>
2025-10-22 11:49:00 +0300

    HW10

--- homework/julia_ikhsanova/hw10/task2.py
+++ homework/julia_ikhsanova/hw10/task2.py
@@ -12,18 +12,3 @@ def example(text):
 
 
 example('print me', 0)
-
-
-# def repeat_me(count=1):               # сюда попадает число повторений
-#     def decorator(func):              # сюда попадает функция example
-#         def wrapper(*args, **kwargs): # это "обёртка", которая запускает функцию
-#             for _ in range(count):    # повторяем count раз
-#                 func(*args, **kwargs)
-#         return wrapper                # возвращаем обёртку
-#     return decorator                  # возвращаем декоратор
-#
-# @repeat_me(count=2)                   # указываем, сколько раз повторять
-# def example(text):
-#     print(text)
-#
-# example("print me")
